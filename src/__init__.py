f2da83ad09acecd92c7b79623cc9a0c23fd82ddb
Chojan Shang <psiace@outlook.com>
2022-10-29 17:13:56 +0800

    feat(marianne): init with a basic arch

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for marianne"""
+# src/__init__.py
+
+__app_name__ = "marianne"
+__version__ = "0.1.0"
---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+"""The marianne entry point script."""
+# src/__main__.py
+
+from . import cli, __app_name__
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+if __name__ == "__main__":
+    main()
